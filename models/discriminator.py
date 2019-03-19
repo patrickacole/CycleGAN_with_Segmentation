a48e6c9dc0b521d655a1d2b1f1b5ba8d692936a2
Patrick Cole <patrickacole@yahoo.com>
2019-03-19 15:02:05 -0700

    Removed useless input parameter for discriminator
    * also updated gitignore with __pycache__

--- models/discriminator.py
+++ models/discriminator.py
@@ -7,15 +7,14 @@ class Discriminator(tf.keras.Model):
     '''
     Discriminator based on PatchGAN discriminator used in CycleGAN paper 
     '''
-    def __init__(self, output_nc, ngf, n_layers=3):
+    def __init__(self, ndf, n_layers=3):
         super(Discriminator, self).__init__(name='discriminator')
-        self.output_nc = output_nc
         self.model = tf.keras.Sequential()
-        self.model.add(layers.Conv2D(filters=ngf, kernel_size=5, strides=2, padding='valid', use_bias=True, data_format='channels_last'))
+        self.model.add(layers.Conv2D(filters=ndf, kernel_size=5, strides=2, padding='valid', use_bias=True, data_format='channels_last'))
         self.model.add(layers.LeakyReLU(alpha=0.2))
         for i in range(1, n_layers):
             mult = min(2 ** (i + 1), 8)
-            self.model.add(layers.Conv2D(filters=ngf*mult, kernel_size=3, strides=2, padding='valid', use_bias=True, data_format='channels_last'))
+            self.model.add(layers.Conv2D(filters=ndf*mult, kernel_size=3, strides=2, padding='valid', use_bias=True, data_format='channels_last'))
             self.model.add(InstanceNorm2D())
             self.model.add(layers.LeakyReLU(alpha=0.2))
         self.model.add(layers.Conv2D(filters=1, kernel_size=3, strides=1, padding='valid', use_bias=True, data_format='channels_last'))             
@@ -24,7 +23,7 @@ class Discriminator(tf.keras.Model):
         return self.model(inputs)
 
 if __name__=="__main__":
-    model = Discriminator(2, 1)
+    model = Discriminator(2)
     t = tf.constant(np.reshape(np.arange(256*256), [1, 256, 256, 1]), dtype=tf.float32)
     #test case, a single 256 x 256 image with 1 channel
     print(model(t).shape)
