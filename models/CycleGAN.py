ec63e68526b5dcfbe176ee53d0a5227bac0c7f1d
Patrick Cole <patrickacole@yahoo.com>
2019-04-14 01:13:48 -0500

    training file pretty much done

--- models/CycleGAN.py
+++ models/CycleGAN.py
@@ -1,5 +1,3 @@
-import numpy as np
-import numpy.linalg as la
 import tensorflow as tf
 from tensorflow.keras import layers
 
@@ -7,7 +5,7 @@ from generator import *
 from discriminator import *
 
 class CycleGAN():
-    def __init__(out_nc, nfg, ndf, n_layers):
+    def __init__(self, out_nc, nfg, ndf, n_layers):
         # G1: X -> Y
         # G2: Y -> X
         self.G1 = Generator(out_nc, nfg)
@@ -15,14 +13,42 @@ class CycleGAN():
         self.D1 = Discriminator(ndf, n_layers)
         self.D2 = Discriminator(ndf, n_layers)
 
-    def gan_loss(y, x, choice=1):
+        # discriminator loss function
+        self.D_Loss = tf.keras.losses.mse
+
+    def gan_loss(self,y, x, choice=1):
         if choice==1:
             return tf.mean(tf.log(self.D1(y))) + tf.mean(tf.log(1 - self.D1(self.G1(x))))
         return tf.mean(tf.log(self.D2(x))) + tf.mean(tf.log(1 - self.D2(self.G2(y))))
 
-    def cycle_loss(y, x):
+    def cycle_loss(self, y, x):
         return tf.mean(tf.norm(self.G2(self.G1(x)) - x, ord=1)) + \
                tf.mean(tf.norm(self.G1(self.G2(y)) - y, ord=1))
 
-    def total_loss(y, x, lmbda):
-        return gan_loss(y, x, choice=1) + gan_loss(y, x, choice=2) + lmbda * cycle_loss(y, x)
+    def total_loss(self, y, x, lmbda):
+        return self.gan_loss(y, x, choice=1) + self.gan_loss(y, x, choice=2) + \
+               lmbda * self.cycle_loss(y, x)
+
+    def discriminator_loss(self, y, x, choice=1):
+        if choice == 1:
+            d1_choice = self.D1(self.G1(x))
+            d1_answer = tf.zeros(d1.shape, tf.float32)
+            loss_fake = self.D_Loss(d1_choice, d1_answer)
+            d1_choice = self.D1(y)
+            d1_answer = tf.ones(d1.shape, tf.float32)
+            loss_true = self.D_Loss(d1_choice, d1_answer)
+        else:
+            d2_choice = self.D2(self.G2(y))
+            d2_answer = tf.zeros(d2.shape, tf.float32)
+            loss_fake = self.D_Loss(d2_choice, d2_answer)
+            d2_choice = self.D2(x)
+            d2_answer = tf.ones(d2.shape, tf.float32)
+            loss_true = self.D_Loss(d2_choice, d2_answer)
+        return 0.5 * (loss_true + loss_fake)
+
+    def save(self):
+        # TODO
+        pass
+
+    def __call__(self, x, y):
+        return self.G1(x), self.G2(y)
--- train.py
+++ train.py
@@ -15,15 +15,13 @@ if __name__ == "__main__":
     param = Params(sys.argv[1])
 
     # load data
-    dataset = tf.data.Dataset.from_tensor_slices()
-    dataset = dataset.shuffle(buffer_size=param.buff_size).batch(param.batch_size)
+    x_train = ...
+    y_train = ...
+    dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    dataset = dataset.shuffle(buffer_size=param.buffer_size).batch(param.batch_size)
 
-    out_nc = param.out_nc
-    ngf = param.ngf
-    ndf = param.ndf
-    n_layers = param.n_layers
-
-    model = CycleGAN(out=out_nc , ngf=ngf , ndf=ndf , n_layers=n_layers)
+    model = CycleGAN(out=param.out_nc, ngf=param.ngf, \
+                     ndf=param.ndf, n_layers=param.n_layers)
     optimizers = tf.optimizers.Adam(param.lr)
 
     for e in range(param.epoch):
@@ -33,11 +31,18 @@ if __name__ == "__main__":
         for i, (data_x, data_y) in enumerate(dataset):
             with tf.GradientTape() as tape:
                 loss = model.total_loss(data_y, data_x, param.lmbda)
-            gradient1 = tape.gradient(loss, (model.G1).trainable_variables)
-            gradient2 = tape.gradient(loss, (model.G2).trainable_variables)
+                loss_d1 = model.discriminator_loss(data_y, data_x, choice=1)
+                loss_d2 = model.discriminator_loss(data_y, data_x, choice=2)
 
-            optimizer.apply_gradients(zip(gradient1, (model.G1).trainable_variables))
-            optimizer.apply_gradients(zip(gradient2, (model.G2).trainable_variables))
+            gradG1 = tape.gradient(loss, (model.G1).trainable_variables)
+            gradG2 = tape.gradient(loss, (model.G2).trainable_variables)
+            gradD1 = tape.gradient(loss_d1, (model.D1).trainable_variables)
+            gradD2 = tape.gradient(loss_d2, (model.D2).trainable_variables)
+
+            optimizer.apply_gradients(zip(gradG1, (model.G1).trainable_variables))
+            optimizer.apply_gradients(zip(gradG2, (model.G2).trainable_variables))
+            optimizer.apply_gradients(zip(gradD1, (model.D1).trainable_variables))
+            optimizer.apply_gradients(zip(gradD2, (model.D2).trainable_variables))
 
             if i%200 == 0:
                 print(f'Training loss at step {i}: {float(loss)}')
