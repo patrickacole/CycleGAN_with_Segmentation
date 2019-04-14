e3f3545b664e8d3d7fca00a727f234a60a010cf9
Andrew Chen <andrewchen686@gmail.com>
2019-04-14 00:51:54 -0500

    Finished initial training functionality

--- train.py
+++ train.py
@@ -3,48 +3,41 @@ import numpy.linalg as la
 import time
 import tensorflow as tf
 from tensorflow.keras import layers
+import sys
 
-from models.generator import *
-from models.discriminator import *
-
-def train(out_nc, ngf, ndf, n_layers):
-    G1 = Generator(out_nc, nfg)
-    G2 = Generator(out_nc, nfg)
-    D1 = Discriminator(ndf, n_layers)
-    D2 = Discriminator(ndf, n_layers)
-
-class Loss():
-    def __init__(out_nc, nfg, ndf, n_layers):
-        self.G1 = Generator(out_nc, nfg)
-        self.G2 = Generator(out_nc, nfg)
-        self.D1 = Discriminator(ndf, n_layers)
-        self.D2 = Discriminator(ndf, n_layers)
-
-    def gan_loss(y, x, choice=1):
-        if choice==1:
-            return tf.mean(tf.log(self.D1(y))) + tf.mean(tf.log(1 - self.D1(self.G1(x))))
-        return tf.mean(tf.log(self.D2(y))) + tf.mean(tf.log(1 - self.D2(self.G2(x))))
-
-    def cycle_loss(y, x):
-        return tf.mean(tf.norm(self.G2(self.G1(x)) - x, ord=1)) + \
-               tf.mean(tf.norm(self.G1(self.G2(y)) - 1, ord=1))
-
-    def loss(y, x, lmbda, choice=1):
-        return gan_loss(y, x, choice=1) + lmbda * cycle_loss(y, x)
+from models.CycleGAN import *
+from utils.params import *
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 train.py <params_path>")
+        exit(1)
+    param = Params(sys.argv[1])
+
     # load data
-    dataset = 
+    dataset = tf.data.Dataset.from_tensor_slices()
+    dataset = dataset.shuffle(buffer_size=param.buff_size).batch(param.batch_size)
 
-    out_nc = 
-    ngf = 
-    ndf = 
-    n_layers = 
-    # model = train(out_nc, ngf, ndf, n_layers)
+    out_nc = param.out_nc
+    ngf = param.ngf
+    ndf = param.ndf
+    n_layers = param.n_layers
 
-    loss = Loss(out= , nfg= , ndf= , n_layers= )
+    model = CycleGAN(out=out_nc , ngf=ngf , ndf=ndf , n_layers=n_layers)
+    optimizers = tf.optimizers.Adam(param.lr)
 
-    for e in epoch:
+    for e in range(param.epoch):
+        print(f'Starting epoch {e+1}')
         epoch_start_time = time.time()
 
-        for i, data in enumerate(dataset):
+        for i, (data_x, data_y) in enumerate(dataset):
+            with tf.GradientTape() as tape:
+                loss = model.total_loss(data_y, data_x, param.lmbda)
+            gradient1 = tape.gradient(loss, (model.G1).trainable_variables)
+            gradient2 = tape.gradient(loss, (model.G2).trainable_variables)
+
+            optimizer.apply_gradients(zip(gradient1, (model.G1).trainable_variables))
+            optimizer.apply_gradients(zip(gradient2, (model.G2).trainable_variables))
+
+            if i%200 == 0:
+                print(f'Training loss at step {i}: {float(loss)}')
