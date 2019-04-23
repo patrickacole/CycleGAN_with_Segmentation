6bbff61f119b7109d108b137e4d81340ee98e543
Patrick Cole <patrickacole@yahoo.com>
2019-04-23 00:49:06 -0500

    updated gan loss to be what is actually used in practice

--- models/CycleGAN.py
+++ models/CycleGAN.py
@@ -30,32 +30,41 @@ class CycleGAN():
         self.D_Loss = tf.keras.losses.mse
 
     def gan_loss(self, y, x, choice=1):
-        if choice==1:
-            return tf.mean(tf.log(self.D1(y))) + tf.mean(tf.log(1 - self.D1(self.G1(x))))
-        return tf.mean(tf.log(self.D2(x))) + tf.mean(tf.log(1 - self.D2(self.G2(y))))
+        # if choice==1:
+        #     return tf.mean(tf.log(self.D1(y))) + tf.mean(tf.log(1 - self.D1(self.G1(x))))
+        # return tf.mean(tf.log(self.D2(x))) + tf.mean(tf.log(1 - self.D2(self.G2(y))))
+        if choice == 1:
+            d1_fake = self.D1(self.G1(x))
+            valid = tf.ones(d1_fake.shape, tf.float32)
+            loss = self.D_Loss(d1_fake, valid)
+        else:
+            d2_fake = self.D2(self.G2(y))
+            valid = tf.ones(d2_fake.shape, tf.float32)
+            loss = self.D_Loss(d2_fake, valid)
+        return loss
 
     def cycle_loss(self, y, x):
         return tf.mean(tf.norm(self.G2(self.G1(x)) - x, ord=1)) + \
                tf.mean(tf.norm(self.G1(self.G2(y)) - y, ord=1))
 
     def total_loss(self, y, x, lmbda):
-        return self.gan_loss(y, x, choice=1) + self.gan_loss(y, x, choice=2) + \
+        return 0.5 * (self.gan_loss(y, x, choice=1) + self.gan_loss(y, x, choice=2)) + \
                lmbda * self.cycle_loss(y, x)
 
     def discriminator_loss(self, y, x, choice=1):
         if choice == 1:
             d1_choice = self.D1(self.G1(x))
-            d1_answer = tf.zeros(d1.shape, tf.float32)
+            d1_answer = tf.zeros(d1_choice.shape, tf.float32)
             loss_fake = self.D_Loss(d1_choice, d1_answer)
             d1_choice = self.D1(y)
-            d1_answer = tf.ones(d1.shape, tf.float32)
+            d1_answer = tf.ones(d1_choice.shape, tf.float32)
             loss_true = self.D_Loss(d1_choice, d1_answer)
         else:
             d2_choice = self.D2(self.G2(y))
-            d2_answer = tf.zeros(d2.shape, tf.float32)
+            d2_answer = tf.zeros(d2_choice.shape, tf.float32)
             loss_fake = self.D_Loss(d2_choice, d2_answer)
             d2_choice = self.D2(x)
-            d2_answer = tf.ones(d2.shape, tf.float32)
+            d2_answer = tf.ones(d2_choice.shape, tf.float32)
             loss_true = self.D_Loss(d2_choice, d2_answer)
         return 0.5 * (loss_true + loss_fake)
 
