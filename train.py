b65c8c66205c08c7ec32329997ec291fe410c71e
Patrick Cole <patrickacole@yahoo.com>
2019-04-25 19:03:16 -0500

    removed double forloop

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from itertools import cycle
 import numpy as np
 import numpy.linalg as la
 import time
@@ -19,8 +20,6 @@ if __name__ == "__main__":
     # load data
     print("Loading data...")
     datasetx, datasety = get_train_dataset(param)
-    datasetx = datasetx.shuffle(buffer_size=param.buff_size).batch(param.batch_size)
-    datasety = datasety.shuffle(buffer_size=param.buff_size).batch(param.batch_size)
 
     print("Creating model...")
     model = CycleGAN(param)
@@ -30,22 +29,23 @@ if __name__ == "__main__":
         print(f'Starting epoch {e+1}')
         epoch_start_time = time.time()
 
-        for i, data_x in enumerate(datasetx):
-            for j, data_y in enumerate(datasety):
-                with tf.GradientTape(persistent=True) as tape:
-                    loss = model.total_loss(data_y, data_x, param.lmbda)
-                    loss_d1 = model.discriminator_loss(data_y, data_x, choice=1)
-                    loss_d2 = model.discriminator_loss(data_y, data_x, choice=2)
+        datasetx = datasetx.shuffle(buffer_size=param.buff_size).batch(param.batch_size)
+        datasety = datasety.shuffle(buffer_size=param.buff_size).batch(param.batch_size)
+        for i, (data_x, data_y) in enumerate(zip(cycle(datasetx), datasety))
+            with tf.GradientTape(persistent=True) as tape:
+                loss = model.total_loss(data_y, data_x, param.lmbda)
+                loss_d1 = model.discriminator_loss(data_y, data_x, choice=1)
+                loss_d2 = model.discriminator_loss(data_y, data_x, choice=2)
 
-                gradG1 = tape.gradient(loss, (model.G1).trainable_variables)
-                gradG2 = tape.gradient(loss, (model.G2).trainable_variables)
-                gradD1 = tape.gradient(loss_d1, (model.D1).trainable_variables)
-                gradD2 = tape.gradient(loss_d2, (model.D2).trainable_variables)
+            gradG1 = tape.gradient(loss, (model.G1).trainable_variables)
+            gradG2 = tape.gradient(loss, (model.G2).trainable_variables)
+            gradD1 = tape.gradient(loss_d1, (model.D1).trainable_variables)
+            gradD2 = tape.gradient(loss_d2, (model.D2).trainable_variables)
 
-                optimizer.apply_gradients(zip(gradG1, (model.G1).trainable_variables))
-                optimizer.apply_gradients(zip(gradG2, (model.G2).trainable_variables))
-                optimizer.apply_gradients(zip(gradD1, (model.D1).trainable_variables))
-                optimizer.apply_gradients(zip(gradD2, (model.D2).trainable_variables))
+            optimizer.apply_gradients(zip(gradG1, (model.G1).trainable_variables))
+            optimizer.apply_gradients(zip(gradG2, (model.G2).trainable_variables))
+            optimizer.apply_gradients(zip(gradD1, (model.D1).trainable_variables))
+            optimizer.apply_gradients(zip(gradD2, (model.D2).trainable_variables))
 
             if i%200 == 0:
                 print(f'Training loss at epoch {e+1} step {i}: {float(loss)}')
