43e1ae5ccee30e41853426c22ec4ab2b6b59e0d8
Patrick Cole <patrickacole@yahoo.com>
2019-04-29 23:34:36 -0500

    Updated data loader to be in PyTorch

--- pytorch/utils/data_loader.py
+++ pytorch/utils/data_loader.py
@@ -1,56 +1,111 @@
-import tensorflow as tf
 import os
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms, datasets
+from PIL import Image
 
-def get_train_dataset(params, pprocess=True):
-    """
-    loads training data
-    @param params   : Params object
-    @return dataset : tf.data.Dataset object
-    """
-    x = tf.data.Dataset.from_tensor_slices(load_dir(params.train_path_a))
-    x_ds = x.map(lambda f: preprocess(f, params.image_size, pprocess=pprocess))
-    y = tf.data.Dataset.from_tensor_slices(load_dir(params.train_path_b))
-    y_ds = y.map(lambda f: preprocess(f, params.image_size, pprocess=pprocess))
-    return x_ds, y_ds
+class ImageFolder():
+    def __init__(self, root, transform=None, train=True):
+        self.root = root
+        self.train = train
+        self.transform = transform
+        files = os.listdir(self.root)
+        self.samples = [os.path.join(self.root, f) for f in files]
+        self.samples.sort()
 
-def get_test_dataset(params, pprocess=True):
-    """
-    loads testing data
-    @param params   : Params object
-    @return dataset : tf.data.Dataset object
-    """
-    x = tf.data.Dataset.from_tensor_slices(load_dir(params.test_path_a))
-    x_ds = x.map(lambda f: preprocess(f, params.image_size, pprocess=pprocess))
-    y = tf.data.Dataset.from_tensor_slices(load_dir(params.test_path_b))
-    y_ds = y.map(lambda f: preprocess(f, params.image_size, pprocess=pprocess))
-    return x_ds, y_ds
+    def loader(self, path):
+        with open(path, 'rb') as f:
+            img = Image.open(f)
+            return img.convert('RGB')
 
-def load_dir(path):
-    """
-    builds list of images names from a given path
-    @param path : string for path to image directory
-    """
-    if not os.path.exists(path):
-        raise IOError(f'Given path {path} does not exist...')
-    fnames = []
-    for file in os.listdir(path):
-        if 'jpg' not in file:
-            continue
-        fnames.append(os.path.join(path, file))
-    return fnames
+    def __len__(self):
+        return len(self.samples)
 
-def preprocess(filename, size, pprocess=False):
-    """
-    transform image to be of range [-1,1] with correct size
-    @param filename  : filename
-    @return image : tensor of shape (size,size,3)
-    """
-    img_raw = tf.io.read_file(filename)
-    #img_tens = tf.image.decode_image(img_raw)
-    image = tf.image.decode_jpeg(img_raw, channels=3)
-    image = tf.image.resize(image, [size, size])
-    if pprocess:
-        image /= 128.0  # normalize to [0,2] range
-        image -= 1.0 # normalize to [-1,1] range
-    return image
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
 
+        if self.train:
+            return sample
+        return path, sample
+
+class DatasetA(Dataset):
+    def __init__(self, params, train=True):
+        self.train = train
+        t = transforms.Compose([ transforms.Resize(params.image_size),
+                                  transforms.ToTensor() ])
+        if self.train:
+            self.data = ImageFolder(root=params.train_path_a, transform=t, train=True)
+        else:
+            self.data = ImageFolder(root=params.test_path_a, transform=t, train=False)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # sample is already in range [0,1] want in range [-1,1]
+        if self.train:
+            sample = self.data[idx]
+            sample = (2 * sample) - 1.0
+            return sample
+        else:
+            path, sample = self.data[idx]
+            sample = (2 * sample) - 1.0
+            return path, sample
+
+class DatasetB(Dataset):
+    def __init__(self, params, train=True):
+        self.train = train
+        t = transforms.Compose([ transforms.Resize(params.image_size),
+                                  transforms.ToTensor() ])
+        if self.train:
+            self.data = ImageFolder(root=params.train_path_b, transform=t, train=True)
+        else:
+            self.data = ImageFolder(root=params.test_path_b, transform=t, train=False)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # sample is already in range [0,1] want in range [-1,1]
+        if self.train:
+            sample = self.data[idx]
+            sample = (2 * sample) - 1.0
+            return sample
+        else:
+            path, sample = self.data[idx]
+            sample = (2 * sample) - 1.0
+            return path, sample
+
+if __name__=="__main__":
+    from argparse import Namespace
+    params = {'image_size':256, 'train_path_a' : 'datasets/monet2photo/trainA', 
+              'train_path_b' : 'datasets/monet2photo/trainB',
+              'test_path_a' : 'datasets/monet2photo/testA',
+              'test_path_b' : 'datasets/monet2photo/testB'}
+    params = Namespace(**params)
+    print('Testing training dataset...')
+    x_train = DatasetA(params, train=True)
+    print(x_train[100])
+
+    print('Testing test dataset...')
+    x_test = DatasetA(params, train=False)
+    print(x_test[100])
+
+    print('Testing in a dataloader...')
+    data = torch.utils.data.DataLoader(x_test, batch_size=4, shuffle=True)
+    for i, d in enumerate(data):
+        filenames = d[0]
+        xs = d[1]
+        print(filenames)
+        print(torch.max(xs))
+        print(torch.min(xs))
+        break
