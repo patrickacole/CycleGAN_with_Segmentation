d4065a1af626f63be1b4bc219eed07f74591df33
Patrick Cole <patrickacole@yahoo.com>
2019-04-29 22:16:51 -0500

    Updated discriminator to be in PyTorch

--- pytorch/models/discriminator.py
+++ pytorch/models/discriminator.py
@@ -1,35 +1,48 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras import layers
-from models.generator import InstanceNorm2D
+import torch
+import torch.nn as nn
 
-class Discriminator(tf.keras.Model):
-    '''
-    Discriminator based on PatchGAN discriminator used in CycleGAN paper 
-    '''
-    def __init__(self, ndf, n_layers=3):
-        super(Discriminator, self).__init__(name='discriminator')
-        self.model = tf.keras.Sequential()
-        self.model.add(layers.Conv2D(filters=ndf, kernel_size=5, strides=2, padding='valid', use_bias=True, data_format='channels_last'))
-        self.model.add(layers.LeakyReLU(alpha=0.2))
+class Discriminator(nn.Module):
+    """
+    Defines a PatchGAN discriminator
+    """
+
+    def __init__(self, input_nc, ndf, n_layers):
+        super(Discriminator, self).__init__()
+
+        model = [nn.Conv2d(input_nc, ndf, 4, stride=2, padding=1),
+                 nn.LeakyReLU(negative_slope=0.2, inplace=True)]
+
+        mult = ndf
         for i in range(1, n_layers):
-            mult = min(2 ** i, 8)
-            self.model.add(layers.Conv2D(filters=ndf*mult, kernel_size=3, strides=2, padding='valid', use_bias=True, data_format='channels_last'))
-            self.model.add(InstanceNorm2D())
-            self.model.add(layers.LeakyReLU(alpha=0.2))
+            mult_prev = mult
+            mult = min(2**i, 8) * ndf
+            model += [nn.Conv2d(mult_prev, mult, kernel_size=4, stride=2, padding=1),
+                      nn.InstanceNorm2d(mult),
+                      nn.LeakyReLU(negative_slope=0.2, inplace=True)]
 
-        mult = min(2 ** n_layers, 8)
-        self.model.add(layers.Conv2D(filters=ndf*mult, kernel_size=3, strides=1, padding='valid', use_bias=True, data_format='channels_last'))
-        self.model.add(InstanceNorm2D())
-        self.model.add(layers.LeakyReLU(alpha=0.2))
+        mult_prev = mult
+        mult = min(2**n_layers, 8)
+        model += [nn.Conv2d(mult_prev, mult, kernel_size=4, stride=1, padding=1),
+                  nn.InstanceNorm2d(mult),
+                  nn.LeakyReLU(negative_slope=0.2, inplace=True)]
 
-        self.model.add(layers.Conv2D(filters=1, kernel_size=3, strides=1, padding='valid', use_bias=True, data_format='channels_last'))
- 
-    def call(self, inputs):
-        return self.model(inputs)
+        model += [nn.Conv2d(mult, 1, kernel_size=4, stride=1, padding=1)]
 
-if __name__=="__main__":
-    model = Discriminator(2)
-    t = tf.constant(np.reshape(np.arange(256*256), [1, 256, 256, 1]), dtype=tf.float32)
-    #test case, a single 256 x 256 image with 1 channel
-    print(model(t).shape)
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        return self.model(x)
+
+if __name__ == "__main__":
+    print("Testing discriminator...")
+    input_nc = 1
+    ndf = 4
+    n_layers = 3
+
+    device = torch.device(("cpu","cuda")[torch.cuda.is_available()])
+
+    model = Discriminator(input_nc, ndf, n_layers).to(device)
+    x = torch.zeros((1,3,256,256), dtype=torch.float32)
+    y = model(x.to(device))
+    print(y.shape)
